d05b7ab840c5a28ae190691b48595aec1be06d9c
Thatuan6900 <thaituan6900@gmail.com>
2023-06-13 15:57:14 +0700

    Code Pagination for User List

--- ThaituanProject/ThaituanWebParent/ThaituanBackEnd/src/main/java/com/thaituan/admin/user/UserController.java
+++ ThaituanProject/ThaituanWebParent/ThaituanBackEnd/src/main/java/com/thaituan/admin/user/UserController.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.util.StringUtils;
@@ -23,10 +24,33 @@ public class UserController {
 	@Autowired
 	private UserService service;
 
+//	@GetMapping("/users")
+//	public String listAll(Model model) {
+//		List<User> listUsers = service.listAll();
+//		model.addAttribute("listUsers", listUsers);
+//		return "users";
+//	}
 	@GetMapping("/users")
 	public String listAll(Model model) {
-		List<User> listUsers = service.listAll();
+		return listByPage(1, model);
+	}
+
+	@GetMapping("/users/page/{pageNum}")
+	public String listByPage(@PathVariable(name = "pageNum") int pageNum, Model model) {
+		Page<User> page = service.listByPage(pageNum);
+		List<User> listUsers = page.getContent();
+
+		long startCount = (pageNum - 1) * UserService.USERS_PER_PAGE + 1;
+		long endCount = startCount * UserService.USERS_PER_PAGE - 1;
+		if (endCount > page.getTotalElements()) {
+			endCount = page.getTotalElements();
+		}
+		
+		model.addAttribute("startCount", startCount);
+		model.addAttribute("endCount", endCount);
+		model.addAttribute("totalItems", page.getTotalElements());
 		model.addAttribute("listUsers", listUsers);
+
 		return "users";
 	}
 
